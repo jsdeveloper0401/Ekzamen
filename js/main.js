fc760e9e8e1ddad61d1945282f210773a21a473c
jsdeveloper0401 <wolfmaks85@gmail.com>
2023-12-06 22:20:46 +0500

    changed mode

--- js/main.js
+++ js/main.js
@@ -12,17 +12,16 @@ setTimeout(() => {
     loading.classList.add("loading-none");
 }, loadingDuration);
 // light-dark start
+// light-dark start
 let modeBtn = document.getElementById("mode__btn");
 
-modeBtn.addEventListener("click", function (e) {
-  if (document.body.className != "dark") {
-    this.firstElementChild.src = "./images/light.svg";
-    console.log(true);
-  } else {
-    this.firstElementChild.src = "./images/dark.svg";
-    console.log(e.target);
-  }
-  document.body.classList.toggle("dark");
+modeBtn.addEventListener("click", function () {
+    if (document.body.className != "dark") {
+        this.firstElementChild.src = "images/light.svg";
+    } else {
+        this.firstElementChild.src = "images/dark.svg";
+    }
+    document.body.classList.toggle("dark");
 });
 // light dark end
 
